7245834842bd5f29278df9773b56a7ae286dbf18
INDIA <INDIA@HARISHDR-PC>
2020-05-26 02:18:43 +0530

    Changes committed

---
+++ springbootharishdemo/src/main/java/com/harish/demo/model/Response.java
@@ -0,0 +1,24 @@
+package com.harish.demo.model;
+
+public class Response {
+
+	private String message;
+	private String accessToken;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getAccessToken() {
+		return accessToken;
+	}
+
+	public void setAccessToken(String accessToken) {
+		this.accessToken = accessToken;
+	}
+
+}
